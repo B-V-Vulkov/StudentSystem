80ed2ff7f43b8e34cd3b95163f8992184403bee3
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2020-01-09 07:01:29 +0200

    Add empty solution - Data

---
+++ StudentSystem/StudentSystem.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentSystem.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=.;Database=StudentSystem;Trusted_Connection=True";
+    }
+}
